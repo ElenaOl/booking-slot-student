68cbe5a745dc632b9d2b1d1a747775b7babc6a15
ElenaOl <pravutiner@gmail.com>
2018-04-01 22:07:43 -0700

    addition of comments

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 
+//I need to pass date to backend and receive not available time slots
+function hideNotAvailableTimes (date){
+    //I need to receive array of booked hours in this format from the backend 
+    var notAvailableTimes = [9, 10, 11, 12, 4];
+        
+        // $.get('url', {
+        //     q: date
+        //   }).done(function(data) {
+        //     var notAvailable = data;
+                $('.time').show();
+                for (var i=0; i<notAvailableTimes.length; i++){
+                    $('#' + notAvailableTimes[i]).hide();
+                }
+                
+        // });
+        
+         
+}
+
+
+//creates datepicker
 $( "#datepicker" ).datepicker({
     beforeShowDay: function(date){ 
         $('.time').hide();
@@ -6,32 +27,40 @@ $( "#datepicker" ).datepicker({
         return [date.getDay() == 6 || date.getDay() == 0,""];
     },
     onSelect: function(date){
-        $('.time').show();
         //removing not available times from showing
-        var notAvailableTimes = getNotAvailableTimes(date);
-        for (var i=0; i<notAvailableTimes.length; i++){
-            $('#' + notAvailableTimes[i]).hide();
-        }
+        hideNotAvailableTimes(date);   
     }
 });
-
+//hiding time picker and summary before choosing the date
 $('.time').hide();
 $('#picked').hide(); 
-function getNotAvailableTimes (date){
-
-    //I need to receive array of hours in this format from the backend
-    return [9, 10, 11, 12, 4];
-}
 
 
+//
 $('.time').on('click', function() {
     $('#picked').show();
     var selectedDate = $('#datepicker').val();
     var selectedTime = this.innerText;
-    $('#date-picked').append(selectedDate);
-    $('#time-picked').append(selectedTime);
+    $('#date-picked').text(selectedDate);
+    $('#time-picked').text(selectedTime); 
 });
 
 $('#submit').on('click', function() {
-    console.log()
+    var selectedDate = $('#date-picked').text();
+    var selectedTime = $('#time-picked').text(); 
+    //call function to update database if not using form html
+    // $.post('url',
+    // {
+    //   date: selectedDate,
+    //   time: selectedTime
+    // },
+    // function(data,status){
+    //     alert("Data: " + data + "\nStatus: " + status);
+    // });
+    
+    // selectedTime = this.innerText;
+    // console.log(datePicked);
+    //update UI
+    $( "#datepicker" ).datepicker( "refresh" );
+    $(datepicker).val('');
 })
